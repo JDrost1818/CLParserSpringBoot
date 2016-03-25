c845f1b148d6fe374d1efdf3d2929dce58897c9a
Drost <JAD0911@daugherty.com>
2016-03-25 10:42:07 -0500

    Embedded test db fixes test errors

--- src/test/java/github/jdrost1818/TestClparserServiceApplication.java
+++ src/test/java/github/jdrost1818/TestClparserServiceApplication.java
@@ -2,6 +2,7 @@ package github.jdrost1818;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,10 +10,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ClparserServiceApplication.class)
 @WebAppConfiguration
-public class ClparserServiceApplicationTests {
+@ActiveProfiles("test")
+public class TestClparserServiceApplication {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
